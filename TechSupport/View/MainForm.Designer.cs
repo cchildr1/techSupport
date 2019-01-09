17b90bdcf48e1599fab13ad229c06c35ad4e88e1
Chris Childress <cchildre@westga.edu>
2019-01-09 13:27:23 -0500

    added MainForm with link and label, added code to pull username from login form and return to login form when link is clicked.

---
+++ TechSupport/View/MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace TechSupport.View
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameLabel = new System.Windows.Forms.Label();
+            this.logoutLink = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // usernameLabel
+            // 
+            this.usernameLabel.AutoSize = true;
+            this.usernameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.usernameLabel.Location = new System.Drawing.Point(607, 13);
+            this.usernameLabel.Name = "usernameLabel";
+            this.usernameLabel.Size = new System.Drawing.Size(93, 20);
+            this.usernameLabel.TabIndex = 0;
+            this.usernameLabel.Text = "User Name:";
+            // 
+            // logoutLink
+            // 
+            this.logoutLink.AutoSize = true;
+            this.logoutLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.logoutLink.Location = new System.Drawing.Point(706, 13);
+            this.logoutLink.Name = "logoutLink";
+            this.logoutLink.Size = new System.Drawing.Size(66, 20);
+            this.logoutLink.TabIndex = 1;
+            this.logoutLink.TabStop = true;
+            this.logoutLink.Text = "Log Out";
+            this.logoutLink.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.logoutLink_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.logoutLink);
+            this.Controls.Add(this.usernameLabel);
+            this.Name = "MainForm";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label usernameLabel;
+        private System.Windows.Forms.LinkLabel logoutLink;
+    }
+}
---
+++ TechSupport/View/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechSupport.View
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm(String userName)
+        {
+            InitializeComponent();
+            usernameLabel.Text = userName;
+        }
+
+        private void logoutLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form loginForm = new LoginForm();
+            this.Close();
+            loginForm.Show();
+        }
+    }
+}
