4bb34cd979bf00d8d89032c87cd7538699a25421
Chris Childress <cchildre@westga.edu>
2019-01-15 15:25:49 -0500

    added model class for Incident containing title, description and customerID

---
+++ TechSupport/Model/Incident.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.Model
+{
+    /// <summary>
+    /// Models an incident which contains a title, description and customerID
+    /// </summary>
+    class Incident
+    {
+        public string Title { get; }
+
+        public string Description { get; }
+
+        public int CustomerID { get; }
+
+        public Incident(string title, string description, int customerID)
+        {
+            if(string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException("Title cannot be null or empty");
+            }
+
+            if(string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentException("Description cannot be null or empty");
+            }
+
+            if (customerID < 0)
+            {
+                throw new ArgumentOutOfRangeException("Customer ID cannot be negative");
+            }
+
+            this.Title = title;
+            this.Description = description;
+            this.CustomerID = customerID;
+        }
+    }
+}
